b144a99d102b8dba9c53d5e171ce71d1dbf36d79
Adam Yates <beethoven123456@yahoo.com>
2016-12-10 21:18:46 -0800

    branch commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// mongoose connection
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/test');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MedScD' });
 });
 
 /* LoginPage router */
-router.get('/appPage', function(req, res, next){
-  res.render('index', { title: 'MedScD'});
+router.get('/appPage', function(req,res,next) {
+  res.render('appPage', { title: 'MedScD' });
 });
 module.exports = router;
